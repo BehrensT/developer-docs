5c5b1c7c7abc3a40fc3cbf389b184b2143cafb2b
rdriscoll-cayan <rdriscoll@cayan.com>
2018-04-12 13:59:51 -0400

    Initial Commit of BoardCardKeyed

---
+++ examples/Merchantware/Credit/Vb/BoardCardKeyed/BoardCardKeyed/Module1.vb
@@ -0,0 +1,40 @@
+﻿Imports BoardCardKeyed.MWCredit
+
+Module Module1
+
+    Sub Main()
+        'Create Soap Client
+        Dim MWCredit As New CreditSoapClient
+
+        'Create Credentials Object
+        Dim Credentials As New MerchantCredentials With {
+        .MerchantName = "TEST MERCHANT",
+        .MerchantSiteId = "XXXXXXXX",
+        .MerchantKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
+        }
+
+        'Create PaymentData Object
+        Dim PaymentData As New PaymentData With {
+        .Source = "KEYED",
+        .CardNumber = "4012000033330026",
+        .ExpirationDate = "1221",
+        .CardHolder = "John Doe",
+        .AvsStreetAddress = "1 Federal St",
+        .AvsZipCode = "02110",
+        .CardVerificationValue = "123"
+        }
+
+        'Create Request Object
+        Dim BoardingRequest As New BoardingRequest
+
+        'Process Request
+        Dim BoardingResponse As VaultBoardingResponse45
+        BoardingResponse = MWCredit.BoardCard(Credentials, PaymentData, BoardingRequest)
+
+        'Display Results
+        Console.WriteLine(" Vault Token: {0} Error Code: {1} Error Message: {2} RFMIQ: {3}", BoardingResponse.VaultToken + vbNewLine, BoardingResponse.ErrorCode + vbNewLine, BoardingResponse.ErrorMessage + vbNewLine, BoardingResponse.Rfmiq + vbNewLine)
+        Console.WriteLine("Press Any Key to Close")
+        Console.ReadKey()
+    End Sub
+
+End Module
